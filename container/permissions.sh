04d1f4cd96032cf529fdeff1e5c9b5f0d26b0a04
Mihail Panagiotopoulos <mihasoftware@gmail.com>
2025-12-12 23:12:29 +0100

    no

--- container/permissions.sh
+++ container/permissions.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 cd ../
 
-composer install
+composer i
 
 npm ci --omit dev
 npm config set cache /tmp --global
-su www-data -s /usr/bin/npm run build
+#su www-data -s /usr/bin/npm run build
+npm run build
 
 npm cache clean --force
 
 rm -rf /app/node_modules/
 
 # Permissions
-chgrp -R www-data storage bootstrap/cache
-chmod -R ug+rwx storage bootstrap/cache
+sudo chgrp -R www-data storage bootstrap/cache
+sudo chmod -R ug+rwx storage bootstrap/cache
